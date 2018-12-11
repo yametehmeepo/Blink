23aeeaa6ad7eee1b1e00ac20c7ce2e41d37c0472
“LXP” <yameteh_meepo@163.com>
2018-12-11 17:17:49 +0800

    like组件/tabbar初始化

---
+++ component/like/like.js
@@ -0,0 +1,33 @@
+// component/like/like.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        like: {
+            type: Boolean,
+            value: true
+        },
+        count: {
+            type: Number,
+            value: 12
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        yesSrc: '/component/images/like.png',
+        noSrc: '/component/images/none-like.png'
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        onLikeTap: function (e) {
+            console.log(e)
+        }
+    }
+})
